db3ff7c9d0200125decbd0676dce84efa381b39b
Erik Ebsen <erik.ebsen@me.com>
2015-02-21 23:54:16 -0600

    Still super janky but Pure is in and navigation goes to correct song and dismisses

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -1,35 +1,48 @@
 (function (window, document) {
 
-    var layout   = document.getElementById('layout'),
-        menu     = document.getElementById('menu'),
-        menuLink = document.getElementById('menuLink');
+  var layout   = document.getElementById('layout'),
+      menu     = document.getElementById('menu'),
+      menuLink = document.getElementById('menuLink');
+      songs    = menu.getElementsByTagName('li');
+      // songs    = menu.getElementsByClassNames('navigation');
 
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
+  function toggleClass (element, className) {
+    var classes = element.className.split(/\s+/),
+        length  = classes.length,
+        i       = 0;
 
-        for(; i < length; i++) {
-          if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-          }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
+    for(; i < length; i++) {
+      if (classes[i] === className) {
+        classes.splice(i, 1);
+        break;
+      }
+    }
+    // The className is not found
+    if (length === classes.length) {
+      classes.push(className);
     }
 
-    menuLink.onclick = function (e) {
-        var active = 'active';
+    element.className = classes.join(' ');
+  }
 
-        e.preventDefault();
-        toggleClass(layout, active);
-        toggleClass(menu, active);
-        toggleClass(menuLink, active);
-    };
+  menuLink.onclick = function (e) {
+    var active = 'active';
+
+    e.preventDefault();
+    toggleClass(layout, active);
+    toggleClass(menu, active);
+    toggleClass(menuLink, active);
+  };
+  for (var j = 0; j < songs.length; j++) {
+    songs[j].onclick = function (e) {
+      var active = 'active';
+
+      // console.log('Odelay!');
+      // e.preventDefault();
+      toggleClass(layout, active); // this toggles the menu
+      // toggleClass(menu, active);
+      // toggleClass(menuLink, active);
+    }
+  }
 
 }(this, this.document));
