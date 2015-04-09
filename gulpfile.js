4b2b8f7ad2f10e3336e10eee89cf374953ab9fbf
Erik Ebsen <erik.ebsen@me.com>
2015-04-08 22:41:25 -0500

    Lots of minor improvments to the task scripts, including adding a dependency (gulp-util) and removing another (gulp-debug)

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
 var browserSync = require('browser-sync');
 var gulp        = require('gulp');
-var debug       = require('gulp-debug');
+var util        = require('gulp-util');
 var harp        = require('harp');
 
-
 gulp.task('commands', function () {
-  console.log('This project has the following commands available:\n\t- Run `gulp serve` to launch a local version of the site.\n\t- Run `gulp compile` to build a version of the website for FTP upload.');
-})
+  util.log('This project has the following commands available:\n- Run `gulp serve` to launch a local version of the site.\n- Run `gulp compile` to build a version of the website to upload to a remote server');
+});
 
 gulp.task('compile', function () {
   var compiledOutput = 'www';
+
   // Call `harp compile` as a pre-step for deployment
   // harp.compile(projectPath [,outputPath] [, callback])
   harp.compile(__dirname, compiledOutput, function (error) {
-    if (error) console.log(error);
+    if (error) util.log(error);
   });
-  console.log('Compiled the website into the \'' + compiledOutput + '\' directory.');
+  util.log('Compiled the website into the \'' + compiledOutput + '\' directory.');
 });
 
 gulp.task('serve', function () {
   var port = 9000;
+
   harp.server(__dirname, { port: port }, function () {
     browserSync({
       notify: false,
@@ -30,7 +31,7 @@ gulp.task('serve', function () {
     });
     gulp.watch('public/**/*.js', function () {
       browserSync.reload('main.js', { stream: true });
-    })
+    });
     gulp.watch('public/**/*.jade', function () {
       browserSync.reload();
     });
